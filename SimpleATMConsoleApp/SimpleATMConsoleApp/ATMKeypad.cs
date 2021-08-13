aed598d0e8c5686e131c14246395606e95188ca0
gccornejo441 <gcornejo441@gmail.com>
2021-08-12 20:36:36 -0700

    moving main method to a more appropriate class ATMKeypad

--- SimpleATMConsoleApp/SimpleATMConsoleApp/ATMKeypad.cs
+++ SimpleATMConsoleApp/SimpleATMConsoleApp/ATMKeypad.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Text.RegularExpressions;
 using System.Collections.Generic;
 using System.Text;
 
@@ -81,11 +82,95 @@ namespace SimpleATMConsoleApp
             }
             return validated;
         }
-        /* 
-                 public PinToAccount(int pin)
+        // Requesting name on card as written
+        public static string requestingName()
         {
-            
+            Console.Write("Please enter your name as written on your credit card: ");
+            string nameOnCard = Console.ReadLine();
+            string cleanNameOnCard = Regex.Replace(nameOnCard, @"[^a-zA-Z\s]", string.Empty);
+
+            while (cleanNameOnCard.Length == 0)
+            {
+                Console.Write("Invalid entry, please try again: ");
+                nameOnCard = Console.ReadLine();
+                cleanNameOnCard = Regex.Replace(nameOnCard, @"[^a-zA-Z\s]", string.Empty);
+            }
+
+            Console.WriteLine(cleanNameOnCard);
+            return cleanNameOnCard;
         }
-         */
+
+        public static string requestingCardNumber()
+        {
+            Console.Write("Please enter your card number: ");
+            string cardNumber = Console.ReadLine();
+            // This validates the card number.
+            bool isCardValid = ATMKeypad.isCardNumberValid(cardNumber);
+
+            return cardNumber;
+        }
+
+        public static string requestingSecurityNumber()
+        {
+            string securityNumber = "";
+
+            Console.WriteLine(" ");
+
+            Console.WriteLine("Please enter the three digit security ");
+            Console.Write("code located on the back of your card: ");
+
+            // This validates the card security number.
+            securityNumber = Console.ReadLine();
+            ATMKeypad.isSecurityCodeValid(securityNumber);
+            Console.WriteLine(" ");
+
+            return securityNumber;
+        }
+
+        public static string requestingExpirationDate()
+        {
+            Console.Write("Please enter your expiration date: ");
+            string expDate = Console.ReadLine();
+
+            if (expDate.Length != 4)
+            {
+                while (expDate.Length != 4 || expDate.Length <= 0)
+                {
+                    Console.Write("Invalid entry, please enter two digits for the month and two digit for the year: ");
+                    expDate = Regex.Replace(Console.ReadLine(), @"[^0-9\s]", string.Empty);
+                    if (expDate.Length == 4) break;
+                }
+            }
+
+            List<char> expDateList = new List<char>();
+
+            foreach (char n in expDate)
+            {
+                expDateList.Add(n);
+            }
+
+            string formattedDate = expDateList[0] + "" + expDateList[1] + "/" + expDateList[2] + expDateList[3];
+            return expDate;
+        }
+
+        public static string requestingPin()
+        {
+            Console.Write("Please enter a four digit security code: ");
+            string pin = Console.ReadLine();
+
+            Regex rgx = new Regex(@"^\d{4}(?:\d{2})?$");
+            bool isPinValid = rgx.IsMatch(pin);
+
+            while (!isPinValid)
+            {
+                Console.Write("Invalid try again: ");
+                rgx = new Regex(@"^\d{4}(?:\d{2})?$");
+                isPinValid = rgx.IsMatch(Console.ReadLine());
+                Console.WriteLine(isPinValid);
+            }
+
+            return pin;
+        }
+
     }
 }
--- SimpleATMConsoleApp/SimpleATMConsoleApp/Program.cs
+++ SimpleATMConsoleApp/SimpleATMConsoleApp/Program.cs
@@ -17,102 +17,17 @@ namespace SimpleATMConsoleApp
 
         }
 
-
-        // Requesting name on card as written
-        public static string requestingName()
-        {
-            Console.Write("Please enter your name as written on your credit card: ");
-            string nameOnCard = Console.ReadLine();
-            string cleanNameOnCard = Regex.Replace(nameOnCard, @"[^a-zA-Z\s]", string.Empty);
-
-            while (cleanNameOnCard.Length == 0)
-            {
-                Console.Write("Invalid entry, please try again: ");
-                nameOnCard = Console.ReadLine();
-                cleanNameOnCard = Regex.Replace(nameOnCard, @"[^a-zA-Z\s]", string.Empty);
-            }
-
-            Console.WriteLine(cleanNameOnCard);
-            return cleanNameOnCard;
-        }
-
-        public static string requestingCardNumber()
-        {
-            Console.Write("Please enter your card number: ");
-            string cardNumber = Console.ReadLine();
-            // This validates the card number.
-            bool isCardValid = ATMKeypad.isCardNumberValid(cardNumber);
-
-            return cardNumber;
-        }
-
-        public static string requestingSecurityNumber()
-        {
-            string securityNumber = "";
-
-            Console.WriteLine(" ");
-
-            Console.WriteLine("Please enter the three digit security ");
-            Console.Write("code located on the back of your card: ");
-
-            // This validates the card security number.
-            securityNumber = Console.ReadLine();
-            ATMKeypad.isSecurityCodeValid(securityNumber);
-            Console.WriteLine(" ");
-
-            return securityNumber;
-        }
-
-        public static string requestingExpirationDate()
-        {
-            Console.Write("Please enter your experation date: ");
-            string expDate = Console.ReadLine();
-                    Console.WriteLine("afdafaf: {0}",expDate.Length);
-
-            if(expDate.Length != 4)
-            {
-                while (expDate.Length != 4 || expDate.Length <= 0)
-                {
-                    Console.Write("Invalid entry, please enter two digits for the month and two digit for the year: ");
-                    expDate = Regex.Replace(Console.ReadLine(), @"[^0-9\s]", string.Empty);
-                    if (expDate.Length == 4) break;
-                }
-            }
-
-            List<char> expDateList = new List<char>();
-
-            foreach (char abc in expDate)
-            {
-                expDateList.Add(abc);
-            }
-
-            string formattedDate = expDateList[0] + "" +  expDateList[1] + "/" + expDateList[2] + expDateList[3];
-            Console.WriteLine(formattedDate);
-
-            return expDate;
-        }
-
-        public static string requestingPin()
-        {
-            Console.Write("Please enter a four digit security code: ");
-            string pin = Console.ReadLine();
-
-            return pin;
-        }
-
-
         public static void RequestingInformation()
-        {
-            
-            string expirationDate = requestingExpirationDate();
-            string nameOnCard = requestingName();
+        {    
+            string nameOnCard = ATMKeypad.requestingName();
 
-            string cardNumber = requestingCardNumber();
+            string cardNumber = ATMKeypad.requestingCardNumber();
 
-            string securityNumber = requestingSecurityNumber();
+            string securityNumber = ATMKeypad.requestingSecurityNumber();
 
+            string expirationDate = ATMKeypad.requestingExpirationDate();
 
-            string pin = requestingPin();
+            string pin = ATMKeypad.requestingPin();
 
             var addAccount = new Account(nameOnCard, cardNumber, securityNumber, expirationDate, pin);
 
