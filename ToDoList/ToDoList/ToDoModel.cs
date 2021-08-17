b5fb7cdd913564036d199afe024d5a56ecbbf795
gccornejo441 <gcornejo441@gmail.com>
2021-08-16 22:10:41 -0700

    Model binding and to do list sample

---
+++ ToDoList/ToDoList/ToDoModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ToDoList
+{
+    public class ToDoModel
+    {
+        public int Number { get; set; }
+        public string Title { get; set; }
+        public string Login { get; set; }
+        public string State { get; set; }
+    }
+}
