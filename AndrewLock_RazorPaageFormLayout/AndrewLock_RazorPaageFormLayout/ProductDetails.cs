27f9253c733f87e14d29db3aae60b4825af708df
gccornejo441 <gcornejo441@gmail.com>
2021-08-18 21:10:25 -0700

    Updated to Razor Page layout using Andew Lock's suggested binding layout for forms.

---
+++ AndrewLock_RazorPaageFormLayout/AndrewLock_RazorPaageFormLayout/ProductDetails.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AndrewLock_RazorPaageFormLayout
+{
+    public class ProductDetails
+    {
+        public string ProductName { get; set; }
+        public decimal SellPrice { get; set; }
+        public int QuantityInStock { get; set; }
+
+        public ProductDetails(string productName, decimal sellPrice, int qty)
+        {
+            ProductName = productName;
+            SellPrice = sellPrice;
+            QuantityInStock = qty;
+        }
+    }
+}
