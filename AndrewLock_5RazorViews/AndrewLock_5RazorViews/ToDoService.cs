df5f9641c479c976eba3a6cb06d79460901a3998
gccornejo441 <gcornejo441@gmail.com>
2021-08-20 21:59:43 -0700

    Andrew Lock's sample on dynamic html: todo list

---
+++ AndrewLock_5RazorViews/AndrewLock_5RazorViews/ToDoService.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AndrewLock_5RazorViews
+{
+    public class ToDoService
+    {
+        public List<ToDoItem> AllItems { get; } = new List<ToDoItem>
+        {
+            new ToDoItem(1, "Buy milk", "Buy eggs", "Buy bread"),
+        };
+    }
+}
