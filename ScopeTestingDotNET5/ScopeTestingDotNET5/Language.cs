5750e9405e3399c6b14f8aa8db92a6bec6828582
gccornejo441 <gcornejo441@gmail.com>
2021-08-27 22:42:50 -0700

    Using generics to create a collection of key to value pairs

---
+++ ScopeTestingDotNET5/ScopeTestingDotNET5/Language.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ScopeTestingDotNET5
+{
+    class Language
+    {
+        public string Value { get; }
+        public string Item { get; }
+        public Language(string value, string item)
+        {
+            Value = value;
+            Item = item;
+        }
+    }
+}
--- ScopeTestingDotNET5/ScopeTestingDotNET5/Program.cs
+++ ScopeTestingDotNET5/ScopeTestingDotNET5/Program.cs
@@ -7,39 +7,27 @@ namespace ScopeTestingDotNET5
     {
         static void Main(string[] args)
         {
-            IDictionary<string, string> openWith = new Dictionary<string, string>();
-
-            openWith.Add("txt", "notepad.exe");
-            openWith.Add("bmp", "paint.exe");
-            openWith.Add("dib", "paint.exe");
-            openWith.Add("rtf", "wordpad.exe");
-
-            try
+            IDictionary<string, Language> _allProducts = new Dictionary<string, Language>
             {
-                openWith.Add("txt", "winword.exe");
-            }
-            catch (ArgumentException)
-            {
-                Console.WriteLine("An element with Key = \"txt\" already exists.");
-            }
+                {"csharp", new Language("csharp", "C#")},
+            };
 
-            // can omit its name when accessing elements.
-            Console.WriteLine("For key = \"rtf\", value = {0}.",
-                openWith["rtf"]);
 
-            openWith["rtf"] = "winword.exe";
-            Console.WriteLine("For key = \"rtf\", value = {0}.",
-                openWith["rtf"]);
+        Language GetLanguage(string name)
+        {
+            if (_allProducts.TryGetValue(name, out var product))
+            {
+                    Console.WriteLine("Get Lang: {0}", product.Item);
+            }
+            return null;
+        }
 
-            if (openWith.TryGetValue("tif", out var value))
-            {
-                Console.WriteLine("For key = \"tif\", value = {0}.", value);
-            }
-            else
-            {
-                Console.WriteLine("Key = \"tif\" is not found.");
-            }
+            Console.WriteLine("Get Language: {0}", GetLanguage("csharp"));
 
         }
     }
 }
+   
+
+
+
