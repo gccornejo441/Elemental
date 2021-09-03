d7fc206698208bddcb972e581e37f5cf25f0021c
gccornejo441 <gcornejo441@gmail.com>
2021-09-02 21:12:32 -0700

    API controllers Fruit for test and weatherForcaset

---
+++ WeatherWebAPI/WeatherWebAPI/Controllers/FruitController.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WeatherWebAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class FruitController : ControllerBase
+    {
+        List<string> _fruit = new List<string>
+        {
+            "Pear",
+            "Lemon",
+            "Peach"
+        };
+
+        [HttpGet("fruit")] // Define the route template to call the action.
+        public IEnumerable<string> Index()
+        {
+            return _fruit;
+        }
+    }
+}
