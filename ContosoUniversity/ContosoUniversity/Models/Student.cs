7c8bbf7eff515461ba4d45a874aab15041308e50
gccornejo441 <gcornejo441@gmail.com>
2021-09-15 21:06:47 -0700

    Creating context

--- ContosoUniversity/ContosoUniversity/Models/Student.cs
+++ ContosoUniversity/ContosoUniversity/Models/Student.cs
@@ -7,5 +7,10 @@ namespace ContosoUniversity.Models
 {
     public class Student
     {
+        public int ID { get; set; } //Primary key
+        public string LastName { get; set; }
+        public string FirstName { get; set; }
+        public DateTime EnrollmentDate { get; set; }
+        public ICollection<Enrollment> Enrollments { get; set; } // Navagation property - holds entities related to this entity
     }
 }
