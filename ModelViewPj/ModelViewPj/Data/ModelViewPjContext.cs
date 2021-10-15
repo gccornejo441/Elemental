eee7551f5aa7b623cb581599a8028c3d847e2d3f
gccornejo441 <gcornejo441@gmail.com>
2021-10-14 21:20:47 -0700

    adding search and filter

---
+++ ModelViewPj/ModelViewPj/Data/ModelViewPjContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using ModelViewPj.Models;
+
+namespace ModelViewPj.Data
+{
+    public class ModelViewPjContext : DbContext
+    {
+        public ModelViewPjContext (DbContextOptions<ModelViewPjContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<ModelViewPj.Models.Movie> Movie { get; set; }
+    }
+}
