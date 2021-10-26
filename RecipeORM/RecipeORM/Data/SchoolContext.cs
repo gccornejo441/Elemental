c3ad400923bd11fad14dabc01be6ea1025fd0b36
gccornejo441 <gcornejo441@gmail.com>
2021-10-25 21:52:23 -0700

    remover junk code

---
+++ RecipeORM/RecipeORM/Data/SchoolContext.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+
+namespace RecipeORM.Data
+{
+    public class SchoolContext : DbContext
+    {
+        public SchoolContext (DbContextOptions<SchoolContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<RecipeORM.Data.Student> Student { get; set; }
+    }
+}
---
+++ RecipeORM/RecipeORM/Pages/Students/Details.cshtml.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+using RecipeORM.Data;
+
+namespace RecipeORM.Pages.Students
+{
+    public class DetailsModel : PageModel
+    {
+        private readonly RecipeORM.Data.SchoolContext _context;
+
+        public DetailsModel(RecipeORM.Data.SchoolContext context)
+        {
+            _context = context;
+        }
+
+        public Student Student { get; set; }
+
+        public async Task<IActionResult> OnGetAsync(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            Student = await _context.Student.FirstOrDefaultAsync(m => m.ID == id);
+
+            if (Student == null)
+            {
+                return NotFound();
+            }
+            return Page();
+        }
+    }
+}
